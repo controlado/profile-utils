8f82fbc2335d9434cf55e22666780c662ef89cf2
balaclava <yangabrielogy@gmail.com>
2023-05-30 16:32:03 -0300

    docs: tipos otimizados
    uso do @typedef
    `JSON` -> `Object`

--- requests.js
+++ requests.js
@@ -7,14 +7,14 @@
 
 /**
  * Função que realiza uma requisição utilizando o método fetch.
- * 
+ *
  * @async
  * @function
  * @param {string} method - O método HTTP a ser utilizado (por exemplo, "GET", "POST", "PUT", "DELETE").
  * @param {string} endpoint - A URL do endpoint da API.
- * @param {JSON} [options] - As opções da requisição.
- * @param {JSON} [options.headers] - Os cabeçalhos da requisição (opcional, padrão é um objeto vazio).
- * @param {JSON} [options.body] - O corpo da requisição (opcional, padrão é um objeto vazio).
+ * @param {Object} [options] - As opções da requisição.
+ * @param {Object} [options.headers] - Os cabeçalhos da requisição (opcional, padrão é um objeto vazio).
+ * @param {Object} [options.body] - O corpo da requisição (opcional, padrão é um objeto vazio).
  * @returns {Promise<Response>} Uma Promise que resolve com os dados da resposta da requisição.
  */
 export async function request(method, endpoint, { headers = {}, body = {} } = {}) {
@@ -35,11 +35,20 @@ export async function request(method, endpoint, { headers = {}, body = {} } = {}
 }
 
 /**
+ * Representa as preferências do jogador.
+ *
+ * @typedef {Object} PlayerPreferences
+ * @property {number[]} challengeIds - Os IDs dos emblemas do perfil.
+ * @property {string} [title] - O título do perfil.
+ * @property {string} [bannerAccent] - O banner do perfil.
+ */
+
+/**
  * Obtém as preferências do jogador.
- * 
+ *
  * @async
  * @function
- * @returns {Promise<JSON>} Uma Promise que resolve com os dados das preferências do jogador.
+ * @returns {Promise<PlayerPreferences>} Uma Promise que resolve com os dados das preferências do jogador.
  */
 export async function getPlayerPreferences() {
     const endpoint = "/lol-challenges/v1/summary-player-data/local-player"
@@ -56,10 +65,10 @@ export async function getPlayerPreferences() {
 
 /**
  * Atualiza as preferências do jogador.
- * 
+ *
  * @async
  * @function
- * @param {JSON} playerPreferences - As preferências do jogador a serem atualizadas.
+ * @param {PlayerPreferences} playerPreferences - As preferências do jogador a serem atualizadas.
  * @returns {Promise<Response>} Uma Promise que resolve com os dados da resposta da requisição.
  */
 export async function updatePlayerPreferences(playerPreferences) {
