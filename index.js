0ee393aabd7ef219c3e3ef723cd9e932e7471423
balaclava <yangabrielogy@gmail.com>
2023-05-30 17:45:59 -0300

    refactor: variáveis mais claras

--- index.js
+++ index.js
@@ -15,27 +15,27 @@ export const plugin = {
 }
 
 async function setupInvisibleBanner() {
-  const container = document.querySelector("div > lol-regalia-profile-v2-element")?.shadowRoot.querySelector("div > lol-regalia-banner-v2-element").shadowRoot.querySelector("div")
-  if (!container || container.hasAttribute("invisible-banner-setup")) { return }
+  const bannerContainer = document.querySelector("div > lol-regalia-profile-v2-element")?.shadowRoot.querySelector("div > lol-regalia-banner-v2-element").shadowRoot.querySelector("div")
+  if (!bannerContainer || bannerContainer.hasAttribute("invisible-banner-setup")) { return }
 
-  container.setAttribute("invisible-banner-setup", "true")
-  container.addEventListener("mouseenter", () => { container.style.opacity = "0.5" })
-  container.addEventListener("mouseleave", () => { container.style.opacity = "1" })
-  container.addEventListener("click", async () => {
+  bannerContainer.setAttribute("invisible-banner-setup", "true")
+  bannerContainer.addEventListener("mouseenter", () => { bannerContainer.style.opacity = "0.5" })
+  bannerContainer.addEventListener("mouseleave", () => { bannerContainer.style.opacity = "1" })
+  bannerContainer.addEventListener("click", async () => {
     const playerPreferences = await requests.getPlayerPreferences()
     playerPreferences.bannerAccent = playerPreferences.bannerAccent === "2" ? "1" : "2"
     await requests.updatePlayerPreferences(playerPreferences)
   })
 }
 
-async function setupCopyBadges() {
-  const container = document.querySelector("div > div.challenge-banner-token-container")
-  if (!container || container.hasAttribute("copy-badges-setup")) { return }
+async function setupBadgesFunctions() {
+  const badgesContainer = document.querySelector("div > div.challenge-banner-token-container")
+  if (!badgesContainer || badgesContainer.hasAttribute("copy-badges-setup")) { return }
 
-  container.setAttribute("copy-badges-setup", "true")
-  container.addEventListener("mouseenter", () => { container.style.opacity = "0.5" })
-  container.addEventListener("mouseleave", () => { container.style.opacity = "1" })
-  container.addEventListener("click", async () => {
+  badgesContainer.setAttribute("copy-badges-setup", "true")
+  badgesContainer.addEventListener("mouseenter", () => { badgesContainer.style.opacity = "0.5" })
+  badgesContainer.addEventListener("mouseleave", () => { badgesContainer.style.opacity = "1" })
+  badgesContainer.addEventListener("click", async () => {
     const playerPreferences = await requests.getPlayerPreferences()
 
     if (!playerPreferences.challengeIds.length) {
@@ -47,7 +47,7 @@ async function setupCopyBadges() {
     playerPreferences.challengeIds = Array(3).fill(firstBadge)
     await requests.updatePlayerPreferences(playerPreferences)
   })
-  container.addEventListener("contextmenu", async () => {
+  badgesContainer.addEventListener("contextmenu", async () => {
     const playerPreferences = await requests.getPlayerPreferences()
 
     if (!playerPreferences.challengeIds.length) {
@@ -63,7 +63,7 @@ async function setupCopyBadges() {
 async function onMutation() {
   const toSetup = [
     setupInvisibleBanner(),
-    setupCopyBadges()
+    setupBadgesFunctions()
   ]
   await Promise.all(toSetup)
 }
